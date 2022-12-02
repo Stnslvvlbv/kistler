31f840f264b455e8d97fefebc527ce9ce8d51eeb
Stnslvvlbv <stnslvvlbv@gmail.com>
2022-12-02 12:34:56 +0700

    open file

---
+++ extract_data.py
@@ -0,0 +1,25 @@
+import os
+
+
+def readTXT(file_name):
+    data = []
+    with open(file_name, 'r') as text:
+        file =file_name.split('/')[-1]
+        print(f'Чтение файла, {file} извлечение данных')
+        START_ROW_DATA = 19
+        row = 0
+        while True:
+            line = text.readline()
+
+            if len(line) < 3:
+                return data
+
+            elif START_ROW_DATA > row:
+                row += 1
+            elif START_ROW_DATA == row:
+                data_row = line.strip().split('	')
+                data.append(data_row)
+
+
+result = readTXT('example/KseniaT 005.txt')
+print('result')
